fcd7d74b74c1fe0b3eebe1189bcbafea01d7d18f
mayw-acast <may.woollcott@acast.com>
2022-12-30 12:35:48 -0500

    basic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+
+const container = {
+  backgroundColor: '#FFE9F9',
+  flex: 1,
+  color: '#c80000',
+  display: 'flex',
+  justifyContent: 'center',
+};
+
+const headerContainer = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'flex-end',
+  margin: '10%',
+};
+
+const headerText = {
+  color: '#C80000',
+  fontSize: 120,
+  margin: 0,
+};
+
+const subHeaderText = {
+  fontSize: 20,
+  marginBottom: '30%',
+};
+
+const appNameText = {
+  fontSize: 36,
+  alignSelf: 'flex-start',
+  marginBottom: 5,
+  marginTop: 0,
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={container} className='App'>
+      <div style={headerContainer}>
+        <p style={headerText}>r street labs</p>
+        <p style={subHeaderText}>applications built for mobile</p>
+        <p style={appNameText}>divii</p>
+        <p style={appNameText}>juju</p>
+        <p style={appNameText}>libris log</p>
+      </div>
     </div>
   );
 }
